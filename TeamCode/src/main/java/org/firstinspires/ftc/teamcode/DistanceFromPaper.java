c240d1849e647c9d8ebf4a48d4f669b6b494ee98
Lukas Werner <lwerner@lshigh.org>
2021-01-19 23:21:50 -0800

    refactor the pipeline for reuse in other op modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceFromPaper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceFromPaper.java
@@ -45,7 +45,7 @@ public class DistanceFromPaper extends LinearOpMode {
           cameraMonitorViewId
         );
     phoneCam.openCameraDevice();
-    PaperColorPipeline pipeline = new PaperColorPipeline(0.5);
+    DistanceFromPaperPipeline pipeline = new DistanceFromPaperPipeline(0.5);
     phoneCam.setPipeline(pipeline);
 
     // Set the viewport renderer to use the gpu so we have better handling
@@ -71,111 +71,11 @@ public class DistanceFromPaper extends LinearOpMode {
         "Total Frame time (ms)",
         phoneCam.getTotalFrameTimeMs()
       );
+      telemetry.addData("W, H (px)", pipeline.bounds.size.width+ ", " +pipeline.bounds.size.height);
+      telemetry.addData("distance (in)", pipeline.computeDistance());
       //telemetry.addData("bounds", pipeline.bounds);
       telemetry.update();
       sleep(100);
-    }
-  }
-
-  class PaperColorPipeline extends OpenCvPipeline {
-
-    private final Mat output = new Mat();
-    private final Mat gray = new Mat();
-    private final Mat threshold = new Mat();
-    private final Mat edges = new Mat();
-    private int thresholdThresh = 200;
-    private int thresholdMaxVal = 255;
-    private int cannyMinThreshold = 120;
-    private int cannyMaxThreshold = 150;
-    private final double pctBoundsThreshold;
-    private final ArrayList<MatOfPoint> contours = new ArrayList<>();
-    private final MatOfPoint2f largestContour2f = new MatOfPoint2f();
-
-    public RotatedRect bounds = new RotatedRect();
-
-    PaperColorPipeline(double pctThreshold) {
-      pctBoundsThreshold = pctThreshold;
-    }
-
-    public PaperColorPipeline(
-      double pctThreshold,
-      int thresholdThresh,
-      int thresholdMaxVal
-    ) {
-      this.pctBoundsThreshold = pctThreshold;
-      this.thresholdThresh = thresholdThresh;
-      this.thresholdMaxVal = thresholdMaxVal;
-    }
-
-    public PaperColorPipeline(
-      double pctThreshold,
-      int thresholdThresh,
-      int thresholdMaxVal,
-      int cannyMinThreshold,
-      int cannyMaxThreshold
-    ) {
-      this.pctBoundsThreshold = pctThreshold;
-      this.thresholdThresh = thresholdThresh;
-      this.thresholdMaxVal = thresholdMaxVal;
-      this.cannyMinThreshold = cannyMinThreshold;
-      this.cannyMaxThreshold = cannyMaxThreshold;
-    }
-
-    @Override
-    public Mat processFrame(Mat input) {
-      contours.clear();
-      Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2GRAY);
-      Imgproc.blur(gray, gray, new Size(3, 3));
-      Imgproc.threshold(
-        gray,
-        threshold,
-        thresholdThresh,
-        thresholdMaxVal,
-        Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU
-      );
-      Imgproc.Canny(threshold, edges, cannyMinThreshold, cannyMaxThreshold);
-      Imgproc.findContours(
-        edges,
-        contours,
-        new Mat(),
-        Imgproc.RETR_LIST,
-        Imgproc.CHAIN_APPROX_SIMPLE
-      );
-      input.copyTo(output);
-      //Imgproc.drawContours(output, contours, -1, new Scalar(225, 0, 0));
-      MatOfPoint largestContour = max();
-
-      if (largestContour != null) {
-        largestContour.convertTo(largestContour2f, CvType.CV_32F);
-        RotatedRect newBounds = Imgproc.minAreaRect(largestContour2f);
-        double sizeDifference = newBounds.size.area() / bounds.size.area();
-        if (
-          sizeDifference > pctBoundsThreshold ||
-          sizeDifference < -pctBoundsThreshold
-        ) {
-          bounds = newBounds;
-        }
-        Imgproc.rectangle(
-          output,
-          newBounds.boundingRect(),
-          new Scalar(0, 255, 0)
-        );
-        Imgproc.rectangle(output, bounds.boundingRect(), new Scalar(255, 0, 0));
-      }
-      return output;
-    }
-
-    private MatOfPoint max() {
-      if (contours.size() > 0) {
-        MatOfPoint largest = contours.get(0);
-        for (MatOfPoint point : contours.subList(1, contours.size())) {
-          if (point.size().area() > largest.size().area()) {
-            largest = point;
-          }
-        }
-        return largest;
-      }
-      return null;
     }
   }
 }
