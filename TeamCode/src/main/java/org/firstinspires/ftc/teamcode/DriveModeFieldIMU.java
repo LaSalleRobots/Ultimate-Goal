2b459515f3f214856e8f74b29f6e2576a2bf3c2e
Lukas Werner <lwerner@lshigh.org>
2021-03-19 08:58:07 -0700

    use better IMU class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveModeFieldIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveModeFieldIMU.java
@@ -1,18 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Gyroscope;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Config
 @TeleOp(name = "Driver Controlled (Field Centric [IMU])", group = "Tests")
 public class DriveModeFieldIMU extends LinearOpMode {
 
-  private Gyroscope imu = null;
+  private BNO055IMU imu = null;
+  private Orientation angles;
+  private Acceleration gravity;
 
   private double getGamepadMoveMagnitude(Gamepad gamepad) {
     return Math.hypot(gamepad.left_stick_x, gamepad.left_stick_y);
@@ -26,13 +33,18 @@ public class DriveModeFieldIMU extends LinearOpMode {
     return Math.asin((gamepad.left_stick_x / gamepad.left_stick_y));
   }
 
-  private double getHeading() {
-    return imu.getAngularVelocity(AngleUnit.RADIANS).xRotationRate;
+  private double getRobotHeading() {
+    angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+    return angles.firstAngle;
   }
 
   @Override
   public void runOpMode() throws InterruptedException {
-    imu = hardwareMap.get(Gyroscope.class, "imu");
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+    parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+    imu = hardwareMap.get(BNO055IMU.class, "imu");
+    imu.initialize(parameters);
     DcMotor fL = hardwareMap.get(DcMotor.class, "fL");
     DcMotor bL = hardwareMap.get(DcMotor.class, "bL");
     DcMotor fR = hardwareMap.get(DcMotor.class, "fR");
@@ -42,22 +54,22 @@ public class DriveModeFieldIMU extends LinearOpMode {
     while (opModeIsActive()) {
       fL.setPower(
         getGamepadMoveMagnitude(gamepad1) *
-        Math.sin(getHeading() + (Math.PI / 4)) +
+        Math.sin(getRobotHeading() + (Math.PI / 4)) +
         getGamepadTurnMagnitude(gamepad1)
       );
       bL.setPower(
         getGamepadMoveMagnitude(gamepad1) *
-        Math.sin(getHeading() - (Math.PI / 4)) +
+        Math.sin(getRobotHeading() - (Math.PI / 4)) +
         getGamepadTurnMagnitude(gamepad1)
       );
       fR.setPower(
         getGamepadMoveMagnitude(gamepad1) *
-        Math.sin(getHeading() + (Math.PI / 4)) -
+        Math.sin(getRobotHeading() + (Math.PI / 4)) -
         getGamepadTurnMagnitude(gamepad1)
       );
       bR.setPower(
         getGamepadMoveMagnitude(gamepad1) *
-        Math.sin(getHeading() - (Math.PI / 4)) -
+        Math.sin(getRobotHeading() - (Math.PI / 4)) -
         getGamepadTurnMagnitude(gamepad1)
       );
     }
